a33ed0a6148a9ba60a8c14ab0130adf19ea1ddfa
Árpád Goretity <h2co3@h2co3.org>
2018-11-30 17:20:02 +0100

    `make clean` after building `libquirc.a`

--- build.rs
+++ build.rs
@@ -1,11 +1,25 @@
+use std::fs;
+use std::env;
 use std::process::Command;
 
 fn main() {
+
     Command::new("make")
-        .args(&["-C", "quirc/"])
-        .spawn()
+        .args(&["libquirc.a", "-C", "quirc/"])
+        .status()
         .expect("couldn't build quirc C library");
 
+    let out_dir = env::var("OUT_DIR")
+        .expect("missing OUT_DIR env var");
+
+    fs::copy("quirc/libquirc.a", out_dir.clone() + "/libquirc.a")
+        .expect("couldn't copy libquirc.a to OUT_DIR");
+
+    Command::new("make")
+        .args(&["clean", "-C", "quirc/"])
+        .status()
+        .expect("couldn't make clean");
+
     println!("cargo:rustc-link-lib=static=quirc");
-    println!("cargo:rustc-link-search=native=quirc/");
+    println!("cargo:rustc-link-search=native={}", out_dir);
 }
